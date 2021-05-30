87261d24f1c1107ecf948cab4b885438ea634dca
Mustafa Uysal <41873800+mustafauyysl@users.noreply.github.com>
2021-05-30 04:45:38 +0300

    Update register.php

--- register.php
+++ register.php
@@ -8,10 +8,10 @@
                 height: 200px;
                 width: 200px;
                 border-radius: 100px;
-                padding: 70px 0;
+                padding: 65px 0;
                 text-align: center;
               ">
-          <h1>götür</h1>
+          <h1 class="font-weight-bold" style="color:#4c3398;font-size: 50px;">götür</h1>
         </div>
         <h1 class="text-white w-75">At your door in minutes</h1>
       </div>
@@ -19,6 +19,13 @@
       <div class="col">
         <form class="login-form" action="./settings/operations.php" method="POST">
           <h4 class="text-center mb-4">Register</h4>
+          <?php
+          if ($_GET['register'] == 'exist_user') {
+          ?>
+            <div class="alert alert-danger" role="alert">
+              This e-mail is used by another user.
+            </div>
+          <?php } ?>
           <div class="form-group">
             <label for="exampleInputEmail1">Name</label>
             <input type="text" name="user_name" required class="form-control" />
@@ -43,8 +50,4 @@
     </div>
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php require_once "footer.php"; ?>
