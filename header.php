b0d653398a3be7ca99da171da97ee639feaf5bf6
Mustafa Uysal <41873800+mustafauyysl@users.noreply.github.com>
2021-05-30 04:45:21 +0300

    Update header.php

--- header.php
+++ header.php
@@ -26,8 +26,8 @@ $isExistUser = $isUser == 0;
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #4c3398">
-        <div class="container">
-            <a class="navbar-brand" href="#" style="color: #ffd300; font-weight: 700;">götür</a>
+        <div class="container-fluid px-5">
+            <a class="navbar-brand" href="<?php if ($getUser['user_authority'] == 1) { ?>products.php<?php } else { ?>home.php<?php } ?>" style=" color: #ffd300; font-weight: 700;">götür</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -36,24 +36,37 @@ $isExistUser = $isUser == 0;
                 <ul class="navbar-nav ml-auto">
                     <?php
                     if ($isExistUser) { ?>
-                        <li class="nav-item">
+                        <li class="nav-item active">
                             <a class="nav-link" href="login.php">
                                 <i class="fas fa-user"></i> Login
                                 <span class="sr-only">(current)</span></a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item active">
                             <a class="nav-link" href="register.php">
                                 <i class="fas fa-user-plus mr-1"></i> Register</a>
                         </li>
                     <?php } else { ?>
+                        <?php if ($getUser['user_authority'] == 1) { ?>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="products.php">
+                                    <i class="fas fa-apple-alt mr-1"></i> Products
+                                    <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="orders.php">
+                                    <i class="fas fa-shopping-cart mr-1"></i> Orders
+                                    <span class="sr-only">(current)</span></a>
+                            </li>
+                        <?php } else { ?>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="my-orders.php">
+                                    <i class="fas fa-shopping-cart mr-1"></i> My Orders
+                                    <span class="sr-only">(current)</span></a>
+                            </li>
+                        <?php } ?>
                         <li class="nav-item active">
                             <a class="nav-link" href="logout.php">
-                                <i class="fas fa-shopping-cart mr-1"></i> Cart
-                                <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="logout.php">
-                                <i class="fas fa-sign-out-alt mr-1"></i> Çıkış Yap
+                                <i class="fas fa-sign-out-alt mr-1"></i> Logout
                                 <span class="sr-only">(current)</span></a>
                         </li>
                     <?php } ?>
